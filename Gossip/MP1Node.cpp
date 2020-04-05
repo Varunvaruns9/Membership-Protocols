2464b6ef031cc34cc15d8fbc21d1b4ae95f54940
varunvaruns9 <varunsinghs2021@gmail.com>
2020-04-06 03:36:06 +0530

    Add Gossip Membership

--- Gossip/MP1Node.cpp
+++ Gossip/MP1Node.cpp
@@ -129,6 +129,14 @@ int MP1Node::introduceSelfToGroup(Address *joinaddr) {
         log->LOG(&memberNode->addr, "Starting up group...");
 #endif
         memberNode->inGroup = true;
+
+        // Add yourself to your member list
+        int id = *(int*)(&memberNode->addr.addr);
+        short port = *(short*)(&memberNode->addr.addr[4]);
+        MemberListEntry self(id, port, memberNode->heartbeat, par->getcurrtime());
+        memberNode->memberList.push_back(self);
+
+        log->logNodeAdd(&(memberNode->addr), &(memberNode->addr));
     }
     else {
         size_t msgsize = sizeof(MessageHdr) + sizeof(joinaddr->addr) + sizeof(long) + 1;
@@ -176,6 +184,9 @@ void MP1Node::nodeLoop() {
     	return;
     }
 
+    // Increase the heartbeat
+    memberNode->heartbeat++;
+
     // Check my messages
     checkMessages();
 
@@ -209,15 +220,122 @@ void MP1Node::checkMessages() {
     return;
 }
 
+// Function to convert an id and a port to an Address
+Address idTOaddr(int id, short port)
+{
+    Address address;
+    memcpy(&(address.addr[0]), &id, sizeof(int));
+    memcpy(&(address.addr[4]), &port, sizeof(short));
+    return address;
+}
+
 /**
  * FUNCTION NAME: recvCallBack
  *
  * DESCRIPTION: Message handler for different message types
  */
 bool MP1Node::recvCallBack(void *env, char *data, int size ) {
-	/*
-	 * Your code goes here
-	 */
+    Member *node = (Member *)env;
+
+    if (((MessageHdr *)data)->msgType == JOINREQ)
+    {
+        // Add new node in coordinator's membership list
+        Address address;
+        long heartbeat;
+        memcpy((char *)&address, (char *)((MessageHdr *)data+1), sizeof(Address));
+        memcpy((char *)&heartbeat, (char *)((MessageHdr *)data+1) + 1 + sizeof(Address), sizeof(long));
+
+        int id = *(int*)(address.addr);
+        int port = *(short*)(address.addr+4);
+
+        MemberListEntry newMember(id, port, heartbeat, par->getcurrtime());
+        node->memberList.push_back(newMember);
+
+        // Create a log for added node
+        log->logNodeAdd(&(node->addr), &(address));
+
+        // Create a JOINREP message for new node
+        size_t sizeList = node->memberList.size();
+        size_t msgsize = sizeof(MessageHdr) + sizeof(sizeList) + sizeList * sizeof(MemberListEntry);
+        MessageHdr *msg = (MessageHdr *) malloc(msgsize * sizeof(char));
+
+        msg->msgType = JOINREP;
+        memcpy((char *)(msg+1), &(sizeList), sizeof(sizeList));
+        char *curr = (char *)(msg+1) + sizeof(sizeList);
+        for (auto entry: node->memberList)
+        {
+            memcpy(curr, (char *)&(entry), sizeof(entry));
+            curr = curr + sizeof(entry);
+        }
+
+        // Send JOINREP to newly added node
+        emulNet->ENsend(&(node->addr), &address, (char *)msg, msgsize);
+
+        free(msg);
+    }
+    else if (((MessageHdr *)data)->msgType == JOINREP)
+    {
+        node->inGroup = 1;
+
+        // Add all elements to your membership list
+        size_t sizeList;
+        char *curr = (char *)((MessageHdr *)data+1);
+
+        memcpy(&sizeList, curr, sizeof(sizeList));
+        curr = curr + sizeof(sizeList);
+
+        MemberListEntry newEntry;
+        for (int i=0;i<sizeList;++i)
+        {
+            memcpy(&newEntry, curr, sizeof(newEntry));
+            curr = curr + sizeof(newEntry);
+            node->memberList.push_back(newEntry);
+
+            Address address = idTOaddr(newEntry.id, newEntry.port);
+
+            log->logNodeAdd(&(node->addr), &(address));
+        }
+    }
+    else if (((MessageHdr *)data)->msgType == HBEAT)
+    {
+        size_t sizeList;
+        char *curr = (char *)((MessageHdr *)data+1);
+
+        memcpy((char *)&sizeList, curr, sizeof(sizeList));
+        curr = curr + sizeof(sizeList);
+
+        MemberListEntry newEntry;
+        newEntry.timestamp = par->getcurrtime();
+
+        // Check and add nodes which do not exist in your membership list
+        for (int i=0;i<sizeList;++i)
+        {
+            memcpy((char *)&newEntry, curr, sizeof(newEntry));
+            curr = curr + sizeof(newEntry);
+
+            bool exists = 0;
+            for (auto &entry: node->memberList)
+            {
+                if (entry.id == newEntry.id and entry.port == newEntry.port)
+                {
+                    // Update heartbeat of existing nodes
+                    if (newEntry.heartbeat > entry.heartbeat)
+                    {
+                        entry = newEntry;
+                    }
+                    exists = 1;
+                }
+            }
+
+            if (!exists)
+            {
+                node->memberList.push_back(newEntry);
+                Address address = idTOaddr(newEntry.id, newEntry.port);
+
+                log->logNodeAdd(&(node->addr), &(address));
+            }
+        }
+    }
 }
 
 /**
@@ -228,10 +346,66 @@ bool MP1Node::recvCallBack(void *env, char *data, int size ) {
  * 				Propagate your membership list
  */
 void MP1Node::nodeLoopOps() {
+    // Update your own heartbeat
+    for (auto &entry: memberNode->memberList)
+    {
+        Address address = idTOaddr(entry.id, entry.port);
+        if (address == memberNode->addr)
+        {
+            entry.heartbeat = memberNode->heartbeat;
+            entry.timestamp = par->getcurrtime();
+        }
+    }
 
-	/*
-	 * Your code goes here
-	 */
+    // Delete nodes after TREMOVE time
+    for (auto it=memberNode->memberList.begin();it!=memberNode->memberList.end();)
+    {
+        if (par->getcurrtime() - it->timestamp > TREMOVE)
+        {
+            Address address = idTOaddr(it->id, it->port);
+            log->logNodeRemove(&(memberNode->addr), &address);
+
+            memberNode->memberList.erase(it);
+        }
+        else
+            ++it;
+    }
+
+    // Send heartbeat to NGOSSIPS random nodes
+    for (int i=0;i<NGOSSIPS;++i)
+    {
+        // Choose a node at random from the memberList
+        MemberListEntry target = memberNode->memberList[rand()%((int)memberNode->memberList.size())];
+        Address address = idTOaddr(target.id, target.port);
+
+        size_t alive = 0;
+        for (auto &entry: memberNode->memberList)
+        {
+            if (par->getcurrtime() - entry.timestamp <= TFAIL)
+                alive++;
+        }
+
+        // Prepare a heartbeat message
+        size_t msgsize = sizeof(MessageHdr) + sizeof(alive) + alive * sizeof(MemberListEntry);
+        MessageHdr *msg = (MessageHdr *)malloc(msgsize * sizeof(char));
+
+        msg->msgType = HBEAT;
+        memcpy((char *)(msg+1), (char *)&alive, sizeof(alive));
+        char *curr = (char *)(msg+1) + sizeof(alive);
+        for (auto entry: memberNode->memberList)
+        {
+            if (par->getcurrtime() - entry.timestamp <= TFAIL)
+            {
+                memcpy(curr, &entry, sizeof(entry));
+                curr = curr + sizeof(entry);
+            }
+        }
+
+        // Send the heartbeat
+        emulNet->ENsend(&(memberNode->addr), &(address), (char *)msg, msgsize);
+
+        free(msg);
+    }
 
     return;
 }
--- Gossip/MP1Node.h
+++ Gossip/MP1Node.h
@@ -20,6 +20,7 @@
  */
 #define TREMOVE 20
 #define TFAIL 5
+#define NGOSSIPS 2
 
 /*
  * Note: You can change/add any functions in MP1Node.{h,cpp}
@@ -31,7 +32,7 @@
 enum MsgTypes{
     JOINREQ,
     JOINREP,
-    DUMMYLASTMSGTYPE
+    HBEAT,
 };
 
 /**
